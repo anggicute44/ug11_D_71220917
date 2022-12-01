e8d19f96fe7866e015cd83ee889f0c431dce0cc7
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2022-12-01 16:18:24 +0700

    Add files via upload

---
+++ Ug11_D_71220917.py
@@ -0,0 +1,28 @@
+print("Halo selamat datang dibioskop!")
+print("dimanakah kamu ingin menonton?")
+print("1) XXI empire")
+print("2) XXI amplaz")
+print("3) XXI jcm")
+print("masukkan pilhanmu ")
+print("mau nonton film apa nih? ada film")
+print("1.frozen")
+print("2.keramat")
+print("KKN di dessa penari")
+print("pilih nomor film")
+print("mau nonton layar bioskop apa")
+print("1.reguler")
+print("2.dolby almos")
+print("3.premiere")
+jjj=int(input("pilih nomor tipe layar:"))
+ghgghgh=int(input("berapa banyak tiket nya? "))
+hdhjd=int(input("mau nonton layar bioskop apa:"))
+def ghdfhjsdj() :
+    print("1. 12.35")
+    print("2. 14.45")
+    print("3. 16.55")
+    print("4. 19.05")
+
+
+
+
+
